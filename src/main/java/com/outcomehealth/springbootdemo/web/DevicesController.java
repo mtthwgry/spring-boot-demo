22797598b471e14ed6c57facab161772aae2813e
Matthew Gray <matt.gray@outcomehealth.com>
2017-12-13 13:00:37 -0800

    working endpoint

---
+++ src/main/java/com/outcomehealth/springbootdemo/web/DevicesController.java
@@ -0,0 +1,21 @@
+package com.outcomehealth.springbootdemo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/devices")
+public class DevicesController {
+
+    @GetMapping
+    String index() {
+        return "Hello world!";
+    }
+
+    @GetMapping("/{deviceId}")
+    String show(@PathVariable String deviceId) {
+        return "Fetching " + deviceId;
+    }
+}
