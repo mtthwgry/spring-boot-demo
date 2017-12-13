7dcbed6d9d9e5c33127b83d2fae51875459fecd8
Matthew Gray <matt.gray@outcomehealth.com>
2017-12-13 14:35:49 -0800

    add some migrations

---
+++ out/production/resources/db/migration/V20171213142600__add_account_id_to_devices.sql
@@ -0,0 +1,1 @@
+ALTER TABLE devices ADD COLUMN account_id bigint(20);
---
+++ src/main/java/com/outcomehealth/springbootdemo/domain/Device.java
@@ -0,0 +1,30 @@
+package com.outcomehealth.springbootdemo.domain;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "devices")
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotBlank
+    @Column(unique = true)
+    @Size(min = 36, max = 36)
+    private String uuid;
+
+    @NotBlank
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @NotBlank
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false)
+    private Timestamp updatedAt;
+}
